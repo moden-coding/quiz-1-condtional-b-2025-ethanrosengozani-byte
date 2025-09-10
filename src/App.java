5b6ca01c737a2f6dce37cc67198a168b059d6c23
Ethan <ethanrosengozani@gmail.com>
2025-09-10 09:13:47 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,27 +1,48 @@
 import java.util.*;
 
- /**
- *  Basic task (up to B+ level):
- *    - Ask the user for a whole number x.
- *    - Print whether x is EVEN or ODD.
- *    - If x is exactly 0, also print "ZERO" on its own line.
+/**
+ * Basic task (up to B+ level):
+ * - Ask the user for a whole number x.
+ * - Print whether x is EVEN or ODD.
+ * - If x is exactly 0, also print "ZERO" on its own line.
  *
- *  Moderate task (up to A level): 
- *    - Ask for a second whole number called total (a positive number).
- *    - Compute x divided by total as a DECIMAL calculation.
- *      * Example: if x = 37 and total = 90, print: "37/90 is 0.4111111111111111"
- *    - If total <= 0, print an error message and skip the percentage.
+ * Moderate task (up to A level):
+ * - Ask for a second whole number called total (a positive number).
+ * - Compute x divided by total as a DECIMAL calculation.
+ * * Example: if x = 37 and total = 90, print: "37/90 is 0.4111111111111111"
+ * - If total <= 0, print an error message and skip the percentage.
  *
- *  Challenge task (up to A+ level):
- *    - If x < 0 or x > 100, print "Wrong number!".
- *    - Otherwise:
- *        Check to see if x is a prime number. A prime number less than
- *        100 should be divisible by itself and 1 but not by 2, 3, 5, or 7.
- *        Print your results
+ * Challenge task (up to A+ level):
+ * - If x < 0 or x > 100, print "Wrong number!".
+ * - Otherwise:
+ * Check to see if x is a prime number. A prime number less than
+ * 100 should be divisible by itself and 1 but not by 2, 3, 5, or 7.
+ * Print your results
  */
 public class App {
-    public static void main(String[] args)  {
-        Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a whole number");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number == 0)
+            System.out.println("Zero");
+        if (number % 2 == 0) {
+            System.out.println("this number is even ");
+        } else {
+            System.out.println("This number is odd");
+        }
+        if(number<0 && number>100){
+            System.out.println("wrong number!");}
+        else if(number % 2 == 0 && number  % 3 == 0 && number % 5 == 0 && number %7 == 0){
+                System.out.println("this is a even number "); }
+        
+        System.out.println("Give a second number");
+        double numbertwo = Integer.valueOf(scanner.nextLine());
+        if(numbertwo <= 0 ){
+            System.out.println("invalid input make it a positive number");}
+        else{
+            System.out.println(number + "/ " + numbertwo + "=" + (number / numbertwo ));}
+        
     }
-}
+}  
